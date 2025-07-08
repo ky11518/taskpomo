e906cba50ed29799de2b8e62682051aa23619110
ky11518 <daichi-shigenobu@kyotoss.co.jp>
2025-07-08 14:56:55 +0900

    GREEN: Add TimerTick event to BasicTimer

--- TaskPomo/BasicTimer.cs
+++ TaskPomo/BasicTimer.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace TaskPomo
 {
     public class BasicTimer
     {
         public bool IsRunning { get; private set; } = false;
         public int ElapsedSeconds { get; private set; } = 0;
+        
+        public event EventHandler<EventArgs>? TimerTick;
 
         public void Start()
         {
